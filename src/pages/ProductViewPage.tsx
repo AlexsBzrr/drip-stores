3d20b06e618a92217cbdf2f49b62a1c87b4e0638
Alex Bezerra <alexbzrr@outlook.com>
2025-04-08 22:47:31 -0300

    Add components

--- src/pages/ProductViewPage.tsx
+++ src/pages/ProductViewPage.tsx
@@ -1,7 +1,12 @@
+import ProductOptions from "../components/ProductOptions";
+
 const ProductViewPage = () => {
   return (
     <div>
       <h1>Product View</h1>
+      <div>
+        <ProductOptions />
+      </div>
     </div>
   );
 };
